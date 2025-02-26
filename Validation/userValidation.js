c56749058dcd27f5b79811ff714abb37f010c155
RaviAhir77 <ravigaiya70@gmail.com>
2025-02-26 23:32:55 +0530

    learning joi validation

--- Validation/userValidation.js
+++ Validation/userValidation.js
@@ -1,5 +1,24 @@
 import Joi from "joi";
 
-const userBaseSchema = {
-    name : Joi.string().min(4).max(20).rule()
+const userBaseSchema = Joi.object({
+    name : Joi.string().min(4).max(20).required()
+    .pattern(/^[A-Za-z\s]+$/)
+    .messages({
+        'string.empty' : 'name is a required',
+        'string.min' : 'min length is {#limit} character of you name',
+        'string.max' : 'max length is {#limit} character of you name',
+        'string.pattern.base' : 'Name can a contain only character and space'
+    }),
+
+    email : Joi.string().email().required().max(20).min(5)
+})
+
+const userData = {name : 'fgsgdf',email : 'ravi@gmail.com'}
+
+const {error,value} = userBaseSchema.validate(userData)
+
+if(error){
+    console.log(error)
+}else{
+    console.log('validation data : ',value)
 }
